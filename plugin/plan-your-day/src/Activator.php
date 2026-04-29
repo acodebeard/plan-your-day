4161507eb6e2a462a1c6223b2b0a07cab7b6e114
acodebeard <alanjfullmer@gmail.com>
2026-04-29 08:55:56 -0700

    Restore legacy settings migration (#80)

--- plugin/plan-your-day/src/Activator.php
+++ plugin/plan-your-day/src/Activator.php
@@ -3,6 +3,7 @@ declare( strict_types=1 );
 
 namespace Acodebeard\PlanYourDay;
 
+use Acodebeard\PlanYourDay\Admin\LegacyConfigMigrator;
 use Acodebeard\PlanYourDay\Settings\Settings;
 
 defined( 'ABSPATH' ) || exit;
@@ -12,5 +13,7 @@ final class Activator {
 		update_option( 'plan_your_day_version', PLAN_YOUR_DAY_VERSION );
 		update_option( 'plan_your_day_schema_version', PLAN_YOUR_DAY_SCHEMA_VERSION );
 		add_option( Settings::OPTION_NAME, Settings::defaults(), '', false );
+
+		( new LegacyConfigMigrator( new Settings() ) )->import_if_recommended();
 	}
 }
