acd6d730208be428c147a686454c8e636cfaa0d4
acodebeard <alanjfullmer@gmail.com>
2026-05-07 13:56:18 -0700

    Skip route rebuild on load more

--- plugin/plan-your-day/src/Rest/PlannerRoutes.php
+++ plugin/plan-your-day/src/Rest/PlannerRoutes.php
@@ -90,9 +90,16 @@ final class PlannerRoutes {
 			return $guard;
 		}
 
+		$append_results_request = ! empty( $request_state['append_results'] );
+		$build_options          = $this->public_trip_waypoint_options( true );
+
+		if ( $append_results_request ) {
+			$build_options['include_trip_waypoints'] = false;
+		}
+
 		$planner_state = $this->planner_state_builder->build(
 			$request_state,
-			$this->public_trip_waypoint_options( true )
+			$build_options
 		);
 		DebugLogger::log(
 			'rest.browse.response',
@@ -108,12 +115,15 @@ final class PlannerRoutes {
 			]
 		);
 
-		return new WP_REST_Response(
-			[
-				'browse' => $this->planner_payload_builder->build_browse_payload( $planner_state ),
-				'route'  => $this->planner_payload_builder->build_route_payload( $planner_state ),
-			]
-		);
+		$response_payload = [
+			'browse' => $this->planner_payload_builder->build_browse_payload( $planner_state ),
+		];
+
+		if ( ! $append_results_request ) {
+			$response_payload['route'] = $this->planner_payload_builder->build_route_payload( $planner_state );
+		}
+
+		return new WP_REST_Response( $response_payload );
 	}
 
 	public function route( WP_REST_Request $request ): WP_REST_Response|WP_Error {
